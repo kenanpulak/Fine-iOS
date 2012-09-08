94f9c9c6f1844db50cd109d0e5b9a053c0507d9d
Kenan Pulak <kepulak@server23.msi.event>
2012-09-08 16:24:55 -0700

    commit

---
+++ Fine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fine
+//
+//  Created by Kenan Pulak on 9/8/12.
+//  Copyright (c) 2012 Kenan Pulak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fine
+//
+//  Created by Kenan Pulak on 9/8/12.
+//  Copyright (c) 2012 Kenan Pulak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
